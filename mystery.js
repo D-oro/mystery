a1ad9130dfb4a3b58609de67cb25e8c649609bdc
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-08-09 09:29:38 +0100

    first commit

---
+++ mystery.js
@@ -0,0 +1,9 @@
+// episode 1: Miss Scarlet
+// episode 2: Professor Plum
+// episode 3: First verdict: Mrs Peacock, Second verdict: Professor Plum
+// episode 4: Miss Scarlet, Professor Plum, Mr Mustard
+// episode 5: murder weapon: Revolver
+// episode 6: Mr White
+// episode 7: Miss Scarlet
+// episode 8: murder weapon: candle stick
+// episode 9: Miss Peacock
